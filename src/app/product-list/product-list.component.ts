770c78eca8c8d4a6b7108837f83c4b8c10ce8fcf
Hanhkhph08082 <kieuhonghanh2000@gmail.com>
2020-05-13 21:38:06 +0700

    4 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -19,23 +19,12 @@ products:Product[];
     private productService:ProductService
   ) { }
 
-  ngOnInit() {
-    // this.products=this.productService.getProducts();
+  ngOnInit() {  
     this.getProducts();
   }
-// showDetail(product){
-//   this.selected=product;
-//   console.log(product);
-// }
-// removeItems(id){
-//   // this.products=this.products.filter(product =>product.id !=id);
-//   // console.log(id)
-//   this.products= this.productService.removeProduct(id);
- 
-// }
+
 getProducts(){
   this.productService.getListProduct().subscribe(data =>{
-    // console.log(data)
     this.products=data;
   })
 }
