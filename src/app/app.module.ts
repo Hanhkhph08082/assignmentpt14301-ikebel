47c1c2df1ddee728955f3bdc9f9d83839fb8d036
Hanhkhph08082 <kieuhonghanh2000@gmail.com>
2020-05-14 20:31:29 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
-import {HttpClientModule} from '@angular/common/http';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductListComponent } from './product-list/product-list.component';
